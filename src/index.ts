e5df43147b5bf4ee4f526ad5b46e6e6ca110eb0b
shuyun.yu <shuyun.yu@cocos.com>
2023-06-07 23:27:30 +0800

    feat complte cli

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,75 @@
 import { input } from "@inquirer/prompts"
-import { ExecException, exec } from "child_process"
 import path from "path"
-import fs from "fs"
+import fs, { mkdirSync, readFileSync, writeFileSync } from "fs"
 import { getCurrentDirName } from "./utils"
 
 const currentProjectName = getCurrentDirName();
 
 async function build () {
 
+  const cwd = process.cwd();
+
   let projectName = await input({ message: `project name: ${currentProjectName} ?` })
   projectName = projectName || currentProjectName;
 
-  console.log("setted project name: ", projectName)
+  const projectDescription = await input({ message: 'project description: ' })
 
-  console.log(__dirname)
+  writePageageJson({ cwd, projectName, projectDescription });
+
+  writeGitIgnore({ cwd })
+
+  writeTsConfig({ cwd })
+
+  writeIndex({ cwd, projectName })
 
 }
 
+function readTemplate (filePath: string) {
+  const templatePath = path.resolve(__dirname, `template/${filePath}`)
+  return readFileSync(templatePath, { encoding: 'utf-8' })
+}
+
+function writeFile (cwd: string, filePath: string, content: string) {
+  const p = path.resolve(cwd, filePath)
+  const dir = path.dirname(p)
+  if (!fs.existsSync(dir)) {
+    mkdirSync(dir)
+  }
+  writeFileSync(p, content, { encoding: 'utf-8' })
+}
+
+function writePageageJson (params: {
+  cwd: string;
+  projectName: string;
+  projectDescription: string;
+}) {
+  const packageJsonStr = readTemplate('package.json')
+
+  const json = packageJsonStr.replace('project-name', params.projectName).replace('project-description', params.projectDescription);
+
+  writeFile(params.cwd, 'package.json', json)
+}
+
+function writeGitIgnore (params: {
+  cwd: string;
+}) {
+  const content = readTemplate('.gitignore')
+  writeFile(params.cwd, '.gitignore', content)
+}
+
+function writeTsConfig (params: {
+  cwd: string
+}) {
+  const content = readTemplate('tsconfig.json')
+  writeFile(params.cwd, 'tsconfig.json', content)
+}
+
+function writeIndex (params: {
+  cwd: string;
+  projectName: string;
+}) {
+  const content = `export const projectName = "${params.projectName}"`;
+  writeFile(params.cwd, 'src/index.ts', content)
+}
+
 build();
-
-// exec('git --version', (err: ExecException | null, stdout: string, stderr: string) => {
-//   console.log(stdout)
-// })
-
-// input({ message: "Enter you name:" }).then(answer => {
-//   console.log("you name:", answer)
-// })
